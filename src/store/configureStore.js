d9e634280bad751e6ae144635f103723da8504c9
Jacob Rhoton <rhoton.jacob@gmail.com>
2021-06-09 21:27:47 -0500

    remove test slice from store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import queryReducer from './querySlice';
 import repoReducer from './repoSlice';
 
 export default configureStore({
     reducer: {
-        query: queryReducer,
         repo: repoReducer,
     },
 });
